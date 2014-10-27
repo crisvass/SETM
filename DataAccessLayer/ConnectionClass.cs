7522f2dd61b030f5f48490644afbbfc584a20b51
Cristina Vassallo <crivassallo@gmail.com>
2014-10-27 21:37:26 +0100

    Adds Required Projects for 3-Tier architecture, Entity Model for Database and Connection Class

---
+++ DataAccessLayer/ConnectionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using System.Data.Entity;
+using System.Data.Common;
+
+namespace DataAccessLayer
+{
+    public class ConnectionClass
+    {
+        public dbTradersMarketplaceEntities Entity { get; set; }
+        public DbTransaction Transaction { get; set; }
+
+        public ConnectionClass()
+        {
+            Entity = new dbTradersMarketplaceEntities();
+        }
+    }
+}
