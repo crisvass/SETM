662483186a926d22a4aa615359c113c7b0fb834a
Cristina Vassallo <crivassallo@gmail.com>
2015-01-03 16:14:36 +0100

    Amended Advanced Search

--- BusinessLayer_WebServices/AdvancedSearchDecorator/ByCategorySearch.cs
+++ BusinessLayer_WebServices/AdvancedSearchDecorator/ByCategorySearch.cs
@@ -21,7 +21,7 @@ namespace BusinessLayer_WebServices.AdvancedSearchDecorator
             ProductsRepository pr = new ProductsRepository();
 
             IEnumerable<Product> result = products;
-            if (CategoryId != null)
+            if (CategoryId != null && CategoryId != Guid.Parse("00000000-0000-0000-0000-000000000000"))
                 result = pr.SearchByCategory(products, CategoryId);
 
             if (AdvancedSearchComponent != null)
